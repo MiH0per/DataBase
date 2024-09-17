92dc58275599e0069723253899424fb5b6890810
MiH0per <maksim.kramarov.99@mail.ru>
2024-09-17 14:17:22 +0500

    Uploaded from cmd

---
+++ Lab_1.sql
@@ -0,0 +1,39 @@
+/* first */
+CREATE DATABASE Lab1; 
+
+\c Lab1
+/* second */
+CREATE TABLE users ( 
+    id SERIAL,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50)
+);   
+
+/* third */
+ALTER TABLE users
+ADD COLUMN isadmin INTEGER;
+
+/* fourth */
+ALTER TABLE users
+ALTER COLUMN isadmin TYPE BOOLEAN;
+
+/* fifth */
+ALTER TABLE users
+ALTER COLUMN isadmin SET DEFAULT FALSE;
+
+/* sixth */
+ALTER TABLE users ADD PRIMARY KEY (id);
+
+/*seventh */
+CREATE TABLE tasks (
+    id SERIAL,
+    name VARCHAR(50),
+    user_id INTEGER
+);
+
+/* eighths */ 
+
+ DROP TABLE tasks;
+
+/* ninth */
+ DROP DATABASE Lab1;
